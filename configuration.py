8b805b69d9f1ba5d6680dee7de061e34585b2445
Katherine Rodriguez <angie.kt.rodriguez23@gmail.com>
2025-04-30 15:18:28 -0500

    Se estandarizaron las rutas CREATE_USER_PATH, KITS_PATH, CARD_PATH

--- configuration.py
+++ configuration.py
@@ -1,3 +1,4 @@
-URL_SERVICE = https://cnt-db3000fb-3d58-4d54-9165-af467a811e23.containerhub.tripleten-services.com
-CREATE_USER_PATH = # Almacena la ruta para crear un usuario o usuaria en esta variable
-KITS_PATH = # Almacena la ruta para crear un kit en esta variable
+URL_SERVICE = "https://cnt-8940216f-c6db-40dc-b214-f30ea0fdbce2.containerhub.tripleten-services.com"
+CREATE_USER_PATH = "/users"
+KITS_PATH = "/kits"
+CARDS_PATH = "/cards"
