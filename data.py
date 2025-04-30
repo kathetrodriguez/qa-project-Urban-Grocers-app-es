d1439feacfb8e349073fff9bbae8e42ac8c1766f
Katherine Rodriguez <angie.kt.rodriguez23@gmail.com>
2025-04-30 17:32:25 -0500

    Se modifico user info--> address por una con datos validos y se agrego el auth_token despues de obtenido.

---
+++ data.py
@@ -0,0 +1,10 @@
+
+card_id_1 = "?cardId=1"
+
+user_info = {
+    "firstName": "Angela",
+    "phone": "+573186542187",
+    "address": "Calle san Antonio"
+    }
+
+auth_token = '0fdbba5f-8594-4d95-a77c-c6e13f5844fd'
