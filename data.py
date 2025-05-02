a4615721925103f3cf8d3e9ec21468a3535f8997
Katherine Rodriguez <angie.kt.rodriguez23@gmail.com>
2025-05-02 17:08:12 -0500

    Se actualizo Data con los datos del usurario con los valores correctos segun los requisitos, se agrego el auth token, se creo el kit body con el producto y se agrego headers, para una mejor organizacion.

--- data.py
+++ data.py
@@ -2,9 +2,24 @@
 card_id_1 = "?cardId=1"
 
 user_info = {
-    "firstName": "Angela",
-    "phone": "+573186542187",
-    "address": "Calle san Antonio"
+    "firstName": "Carlina",
+    "phone": "+573118965478",
+    "address": "Calle de la fatiga, 123"
     }
 
-auth_token = '0fdbba5f-8594-4d95-a77c-c6e13f5844fd'
+auth_token = '22794c69-f6e0-4497-a53f-8589318fe287'
+
+kit_body = {
+       "name": "La colombia",
+       "card": {
+           "id": 8,
+           "name": "Desde Bogota"
+       },
+       "productsList": "helado",
+       "id": 2,
+       "productsCount": 0
+   }
+
+headers = {
+    "Content-Type": "application/json"
+}
