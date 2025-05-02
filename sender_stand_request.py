a47390267b5efc48e3ad830dad61868ee467a817
Katherine Rodriguez <angie.kt.rodriguez23@gmail.com>
2025-05-02 17:14:53 -0500

    Se definieron las funciones para crear el usuario, obtener el contenido de la tabla de usuarios, crear un kit nuevo, imprimiendo las respuestas de cada una de las acciones.

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -1,6 +1,8 @@
 import requests
-from data import user_info, card_id_1, auth_token
-from configuration import URL_SERVICE, CREATE_USER_PATH, KITS_PATH
+from data import user_info, card_id_1, auth_token, kit_body, headers
+from configuration import URL_SERVICE, CREATE_USER_PATH, KITS_PATH, USERS_TABLE_PATH
+
+#Creacion de usuario
 
 def create_user():
     url = URL_SERVICE + CREATE_USER_PATH
@@ -20,3 +22,26 @@ except ValueError:
     print("Error al decodificar JSON. Respuesta fue:")
     print(response.text)
 
+# Función para consultar la tabla de los usuarios
+
+def get_users_table():
+    return requests.get(URL_SERVICE + USERS_TABLE_PATH)
+
+
+response_user_table = get_users_table()
+
+print(response_user_table.status_code)
+print(response_user_table.text)
+
+# Función para crear un kit nuevo
+def create_kit(kit):
+
+    current_headers = headers.copy()
+    current_headers["Authorization"] = "Bearer " + response.json()["authToken"]
+    return requests.post(URL_SERVICE + KITS_PATH,
+                         json= kit,
+                         headers= current_headers)
+
+response_kit = create_kit(kit_body)
+print(response_kit.status_code)
+print(response_kit.text)
